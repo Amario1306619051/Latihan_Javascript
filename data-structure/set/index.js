e35181175822f8b38dd2b6ca12a136ed22ab5ac7
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-04-26 23:53:16 +0700

    add di set

--- data-structure/set/index.js
+++ data-structure/set/index.js
@@ -2,4 +2,6 @@ const alexanderArnold = new Set([1,2,3,4,4,4,5,6,6,6,]);
 console.log(alexanderArnold);
 
 const andrewRobertson = new Set(['gajah','semut','kelinci','gajah','burung'])
+andrewRobertson.add('lalat')
+andrewRobertson.add('gajah')
 console.log(andrewRobertson)
