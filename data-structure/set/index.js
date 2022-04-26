8bc3fe097a9810f1ab7df1bbf9eb8c32f4ff1826
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-04-26 23:40:34 +0700

    keren

--- data-structure/set/index.js
+++ data-structure/set/index.js
@@ -1,13 +1,5 @@
-const number_set = new Set([1, 2, 3, 8, 4, 2, 8]);
-const animal_set = new Set(['dog', 'cat', 'elephant']);
+const alexanderArnold = new Set([1,2,3,4,4,4,5,6,6,6,]);
+console.log(alexanderArnold);
 
-// deklarasi array
-const number_array = [1, 2, 3, 8, 4, 2, 8]
-const animal_array = ['dog', 'cat', 'elephant']
-
-// Jika kita print number_array dan animal_array, hasilnya akan sama dengan
-// yang dideklarasikan, namun apa yang akan dihasilkan set?
-
-console.log(number_array); // result: 1, 2, 3, 8, 4, 2, 8
-console.log(animal_array); // result: 'dog', 'cat', 'elephant'
-console.log(number_set);
+const andrewRobertson = new Set(['gajah','semut','kelinci','gajah','burung'])
+console.log(andrewRobertson)
