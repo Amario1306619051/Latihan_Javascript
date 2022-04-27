fa6c773518abfe89c14ecbfe38df39c3558e9b61
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-04-27 16:53:53 +0700

    benerin

--- data-structure/stack/index1.js
+++ data-structure/stack/index1.js
@@ -19,32 +19,25 @@ class TextEditor {
     }
 
     undo() {
-        this.memory.push(this.data[this.top])
-        this.top -= 1
-        // TODO: answer here
-        return this.data.pop()
+        if (this.data.length > 0){
+            this.memory.push(this.data[this.top])
+            this.top -= 1
+            return this.data.pop()
+        }
     }
     
     redo() {
         // TODO: answer here
-        this.top += 1
-        let bisa = this.memory[this.memory.length - 1]
-        this.memory.pop()
-        return this.data.push(bisa)
+        if(this.memory.length > 0){
+            this.top += 1
+            let bisa = this.memory[this.memory.length - 1]
+            this.memory.pop()
+            return this.data.push(bisa)
+        }
+        else{
+            this.top += 0
+        }
+
     }
 }
 
-const x = new TextEditor()
-x.write('a')
-x.write('m')
-x.write('a')
-x.write('r')
-x.write('i')
-x.write('o')
-console.log(x.read())
-x.undo()
-x.undo()
-console.log(x.read())
-x.redo()
-x.redo()
-console.log(x.read())
