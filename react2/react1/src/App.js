5a3a8f8e0546f25bb72d0e83308ff3ef5a161b56
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-10 12:25:08 +0700

    react 2 part 3

--- react2/react1/src/App.js
+++ react2/react1/src/App.js
@@ -1,9 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <p>Amario</p>
     </div>
   );
