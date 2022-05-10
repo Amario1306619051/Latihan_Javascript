e82c1d55a999e66d12d2f5f81ab44ce4538a060e
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-10 17:23:42 +0700

    todo list

---
+++ reactApp/react2/src/TodoList/TodoList.js
@@ -0,0 +1,17 @@
+import "./TodoList.css"
+import ".././Todo/Todo.js"
+import Todo from ".././Todo/Todo.js"
+
+const TodoList = () =>{
+    return(
+        <div>
+            <li>
+                <ul>Amario</ul>
+                <ul>Fausta</ul>
+                <ul>Harlastputra</ul>
+            </li>
+        </div>
+    )
+}
+
+export default TodoList;
