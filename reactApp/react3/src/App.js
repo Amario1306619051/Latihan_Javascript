31ee926d001ba5a1e5cfe3c5b4f88b6456d14234
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-10 22:43:29 +0700

    react3_header_berita with css

--- reactApp/react3/src/App.js
+++ reactApp/react3/src/App.js
@@ -1,5 +1,21 @@
 import './App.css';
 
+import React from 'react'
+
+export const  News1 = () => {
+  return (
+    <div className='News1'>
+      <h2>Erling Halaand ke manchester city !!</h2>
+      <p>Menurut kabar terbaru, penyerang Borussia Dortmund, Erling Haaland, 
+        akan bergabung dengan Manchester City pada musim panas 2022.
+      </p>
+      <p>Raksasa Liga Inggris itu siap untuk mengaktifkan klausul pelepasan sang bomber 
+        sebesar 63 juta pounds atau setara Rp1,1 triliun.
+      </p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
