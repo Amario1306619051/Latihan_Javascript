b81894aba175a01cba3c1c923de90dfce80f0b77
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-10 17:30:55 +0700

    todo list

--- reactApp/react2/src/TodoList/TodoList.js
+++ reactApp/react2/src/TodoList/TodoList.js
@@ -4,11 +4,11 @@ import Todo from ".././Todo/Todo.js"
 const TodoList = () =>{
     return(
         <div>
-            <li>
-                <ul>Amario</ul>
-                <ul>Fausta</ul>
-                <ul>Harlastputra</ul>
-            </li>
+            <ul>
+                <li>Amario</li>
+                <li>Fausta</li>
+                <li>Harlastputra</li>
+            </ul>
         </div>
     )
 }
