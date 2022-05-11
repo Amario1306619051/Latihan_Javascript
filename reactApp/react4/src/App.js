f5d00a70715036ae1680550f7919c7b61070356b
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-11 08:39:58 +0700

    reract4

--- reactApp/react4/src/App.js
+++ reactApp/react4/src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Header = () => {
   return (
     <div>
-      <header>
         Jumlah Tekan pada button 
-      </header>
     </div>
   )
 }
 
 
+export const tombolAneh = () => {
+  const [number, setNumber] = useState(0)
+  
+  return (
+    <div>
+      <p> Jumlah klik : {number} kali klik</p>
+      <button onClick={() => setNumber(number + 1)}>Klik disini untuk nambah</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <tombolAneh />
     </div>
   );
 }
