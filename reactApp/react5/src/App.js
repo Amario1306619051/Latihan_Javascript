c05f63b85d5220ef3a9d7c9c49bd50679f8168f5
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-11 10:46:27 +0700

    react5

--- reactApp/react5/src/App.js
+++ reactApp/react5/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <div className='satu'>
+      <h1>Breaking news</h1>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Kepala'>
+        <Header />
+      </div>
     </div>
   );
 }
