11c32fcdea69f9cfa840663ff0ba95e63d6ebc8a
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-11 08:32:01 +0700

    react4

--- reactApp/react4/src/App.js
+++ reactApp/react4/src/App.js
@@ -1,5 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+
+
+export default function Header() {
+  return (
+    <div>App</div>
+  )
+}
+
 
 function App() {
   return (
