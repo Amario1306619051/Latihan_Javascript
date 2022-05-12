ef6a3c9859fa3de2c33a3327faddfb1ec8723e34
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 23:55:20 +0700

    react15 without logic

--- reactApp/react15/src/component/tombol.js
+++ reactApp/react15/src/component/tombol.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 class tombol extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 0
+        }
+    }
+
   render() {
     return (
-      <div>tombol</div>
+      <div>
+          <h2>Angka : {this.state.number} </h2>
+          <button> + </button>
+          <button> - </button>
+      </div>
     )
   }
 }
