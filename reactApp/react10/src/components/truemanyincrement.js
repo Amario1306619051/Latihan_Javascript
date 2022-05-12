13ca2dacace37fb72e8fa9ec588783ca0f0e75f1
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 13:04:33 +0700

    react10 part 12

---
+++ reactApp/react10/src/components/truemanyincrement.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default class manyincrements extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    OneIncrement = () => {
+        this.setState({ count: this.state.count + 1})
+    }
+
+    FiveIncrement = () => {
+        this.OneIncrement()
+        this.OneIncrement()
+        this.OneIncrement()
+        this.OneIncrement()
+        this.OneIncrement()
+    }
+
+  render() {
+    return (
+      <div>
+          <h1>{this.state.count}</h1>
+          <button onClick={this.FiveIncrement}> Increment </button>
+      </div>
+    )
+  }
+}
