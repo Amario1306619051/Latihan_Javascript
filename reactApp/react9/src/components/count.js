b01bb272de7afdf75db54e1eb9d418a9ec7dea76
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 12:33:24 +0700

    count react9 fix it git add .

--- reactApp/react9/src/components/count.js
+++ reactApp/react9/src/components/count.js
@@ -4,18 +4,22 @@ export default class count extends Component {
     constructor (props){
         super(props)
         this.state = {
-            count: 0 
+            count: 0,
+            brother: 0
         }
     }
 
     Nambah = () => {
-        this.setState({count: this.state.count + 1})
+        this.setState({count: this.state.count + 1},() =>{
+            brother : this.state.brother + 1
+        })
     }
   render() {
     return (
       <div> 
           <h3>{this.state.count}</h3>
           <button onClick={this.Nambah}> Add Count number</button>
+          <p>this.state.brother</p>
       </div>
     )
   }
