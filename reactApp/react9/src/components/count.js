eda7ffc23f9d24ebb3e981c25c40fb37bd4f065a
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 12:20:05 +0700

    count react 9

---
+++ reactApp/react9/src/components/count.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class count extends Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            count: 0 
+        }
+    }
+  render() {
+    return (
+      <div>
+          count
+      </div>
+    )
+  }
+}
