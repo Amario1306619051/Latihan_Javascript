1b9997cf81015f0f5d4ec434bd92e5139d57207c
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 19:50:24 +0700

    react13 complete

--- reactApp/react13/src/component/angka.js
+++ reactApp/react13/src/component/angka.js
@@ -7,12 +7,24 @@ export default class angka extends Component {
             number: 4
         }
     }
+
+    Tambah = () => {
+        this.setState(
+            {number : this.state.number + 1}
+        )
+    }
+
+    Kurang = () => {
+        this.setState(
+            {number: this.state.number - 1}
+        )
+    }
   render() {
     return (
       <div>
           <h1> {this.state.number}</h1>
-          <button> - </button>
-          <button> + </button>
+          <button onClick={this.Kurang}> - </button>
+          <button onClick={this.Tambah}> + </button>
       </div>
     )
   }
