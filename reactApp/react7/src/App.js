02a52ffd7fb6d96bdc38bc104f0e22ee091da224
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 11:20:08 +0700

    real react7 part 8

--- reactApp/react7/src/App.js
+++ reactApp/react7/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import React from 'react'
 import satu from "./components/greetings"
 
+const greetings = (props) => {
+  return (
+    <div>greetings</div>
+  )
+}
+
 export class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <h1>Hello World</h1>
-        <satu />
+        <greetings />
       </div>
 
     )
