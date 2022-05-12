5ede077262e044e899c3f15a210fe5aab16e2348
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 20:36:23 +0700

    react14

--- reactApp/react14/src/component/button.js
+++ reactApp/react14/src/component/button.js
@@ -1,34 +1,21 @@
 import React, {useState} from 'react'
 
-export default function button() {
+function button() {
 
-    const [count,setCount] = useState(0)
+    const [count, setCount] = useState(0)
 
-    Tambah = () =>{
-        setCount(count + 1)
-    }
-
-    Kurang = () =>{
-        setCount(count - 1)
-    }
-
-    IncrementFive = () =>{
-        for(let i = 0; i == 6; i ++){
+    const IncrementFive = () =>{
+        for(let i = 0; i == 5; i ++){
             setCount(prevCount => prevCount + 1)
         }
-    }
-
-    Reset = () => {
-        setCount(0)
     }
 
   return (
     <div>
         <h1> Jumlah angka {count} </h1>
-        <button onClick={Tambah}> Tambah </button>
-        <button onClick={Kurang}> Kurang </button>
-        <button onClick={IncrementFive}> Tambah 5</button>
-        <button onClick={Reset}> Reset </button>
+        <button onClick={setCount(count + 1)}> Tambah </button>
+        <button onClick={setCount(count - 1 )}> Kurang </button>
+        <button onClick={setCount(count = 0)}> Reset </button>
     </div>
   )
 }
