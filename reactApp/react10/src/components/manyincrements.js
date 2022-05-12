4479773bf281fd43010e2c37670eab48e9b01c73
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 12:50:09 +0700

    react10 part 2

---
+++ reactApp/react10/src/components/manyincrements.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class manyincrements extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+
+    oneIncrement = () => {
+
+    }
+  render() {
+    return (
+      <div>
+          <h1> Hitung Hitugan</h1>
+          <button onClick={() =>}> Increment </button>
+      </div>
+    )
+  }
+}
