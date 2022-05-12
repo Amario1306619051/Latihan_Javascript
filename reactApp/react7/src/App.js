2418514bc0a08c08ee5b12c7e7a6b0eeec69f5fe
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-12 12:06:44 +0700

    react7 final

--- reactApp/react7/src/App.js
+++ reactApp/react7/src/App.js
@@ -1,22 +1,15 @@
 import './App.css';
 import React from 'react'
-import satu from "./components/greetings"
+import Satu from "./components/greetings"
 
-const greetings = (props) => {
-  return (
-    <div>greetings</div>
-  )
-}
 
 export class App extends React.Component {
   render() {
     return (
       <div className='App'>
         <h1>Hello World</h1>
-        <greetings />
-        <satu />
+        <Satu name = "Amario" country='Indonesia'/>
       </div>
-
     )
   }
 }
--- reactApp/react7/src/components/greetings.js
+++ reactApp/react7/src/components/greetings.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const greetings = (props) => {
   return (
-    <div>greetings</div>
+    <div>
+      Hello {this.props.name} you are form {this.props.country}
+    </div>
   )
 }
 
