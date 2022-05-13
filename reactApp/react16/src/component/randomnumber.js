1b5f66556f29ea3736d55397ba810dc84ce9ffb6
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-13 21:59:33 +0700

    pembuatan randomnumber.js

---
+++ reactApp/react16/src/component/randomnumber.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class randomnumber extends Component {
+  render() {
+    return (
+      <div>
+          randomnumber
+        </div>
+    )
+  }
+}
