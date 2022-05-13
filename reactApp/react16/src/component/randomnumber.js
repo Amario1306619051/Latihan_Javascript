8f2d5a169b42d0403aca0663382fbad9a052cb0d
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-13 22:18:14 +0700

    random qoute

--- reactApp/react16/src/component/randomnumber.js
+++ reactApp/react16/src/component/randomnumber.js
@@ -123,14 +123,15 @@ export default class randomnumber extends Component {
     Acak = () =>{
         this.setState(
             {
-                number: Math.floor(Math.random()*this.state.emoji.length)
+                number: Math.floor(Math.random()*this.state.qoute.length)
             }
         )
     }
   render() {
     return (
       <div>
-        <h2> Generate Random number : {this.state.emoji[this.state.number]}</h2>
+        <p>{this.state.number}</p>
+        <h2> Generate Random number : {this.state.qoute[this.state.number]}</h2>
         <button onClick={this.Acak}>Generate me</button>
       </div>
     )
