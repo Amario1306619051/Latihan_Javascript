0c8beab09b0b6c7c46b238af0c30b346623813a0
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-15 01:13:13 +0700

    react17 css and jx

---
+++ reactApp/react17/src/component/tombol.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export class tombol extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+        message: 'Hello'
+      }
+    }
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default tombol
