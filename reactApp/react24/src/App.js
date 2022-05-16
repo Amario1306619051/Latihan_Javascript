c02847057aa22bb1f9db497ca17e23b57a560939
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-16 22:13:09 +0700

    react 24 list rendering

--- reactApp/react24/src/App.js
+++ reactApp/react24/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Siap from './component/satu'
 
 function App() {
   return (
     <div className="App">
       <h1> Hello world</h1>
+      <Siap />
     </div>
   );
 }
---
+++ reactApp/react24/src/component/satu.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Eksfis () {
+    const names = ['Amario','Muhlis','Pridana']
+  return (
+    <div>
+        {
+           names.map(name => <h2>name</h2>) 
+        }
+    </div>
+  )
+}
+
+export default Eksfis
