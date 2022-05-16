5f8a1490c54cfb398aef54c3ac3d47a3dd0715ac
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-16 16:32:07 +0700

    react22 membuat component userGreeting (2)

---
+++ reactApp/react22/src/component/userGreeting.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class userGreeting extends Component {
+  render() {
+    return (
+      <div>
+          Hello Amario
+      </div>
+    )
+  }
+}
