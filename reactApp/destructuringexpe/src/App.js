98e7c53e4b1f91948a933f9cdcb971e285721671
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-17 23:29:31 +0700

    destructing

--- reactApp/destructuringexpe/src/App.js
+++ reactApp/destructuringexpe/src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
 
 Greet = ({name, country}) => {
-  
+  return(
+    <div>
+      <h3>Hello i am {name} from {country}</h3>
+    </div>
+  )
 }
 
 function App() {
   return (
     <div className="App">
-      
+      <Greet name="Amario" from = "Indonesia" />
+      <Greet name="Jimin" from = "Korea" />
     </div>
   );
 }
