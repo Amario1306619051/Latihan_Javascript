d90cb77f003e91a67981c9183d0f1882e4472b8b
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-17 21:45:49 +0700

    usestatefunction done

--- reactApp/usestatefunction1/src/App.js
+++ reactApp/usestatefunction1/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
+
+function Keren() {
+  const [count, setCount] = useState(0)
+
+  
+  return(
+    <div>
+      <h2>Hello visitors : {count}</h2>
+      <button onClick={() => setCount(count+1)}> klik me</button>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Keren />
     </div>
   );
 }
