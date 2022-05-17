4cd70a3bcc4956c699a087924cf1e2e639bf8238
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-18 00:07:14 +0700

    dummy

---
+++ reactApp/dumybuttonproject/src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import React, { Component } from 'react'
+
+
+function BolTombol(props) {
+  return (
+    <div>
+      <button> {props.name}</button>
+    </div>
+  )
+}
+
+
+class Logstat extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       stat: false
+    }
+
+  }
+
+  render() {
+     let message
+     if (this.state.stat === false){
+       message = <div>
+         <h2>Lets choice the users</h2>
+         <BolTombol name="Pridana" />
+         <BolTombol name="Amario" />
+         <BolTombol name="Muhlis" />
+         
+       </div>
+     }
+     else{
+
+     }
+     return(
+       <div>{message}</div>
+     )
+  }
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello visitors</h1>
+      <Logstat />
+    </div>
+  );
+}
+
+export default App;
