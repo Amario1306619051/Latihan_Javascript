d137d715a6773069b4648a737092e2b454b6bff7
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-17 22:17:35 +0700

    try addedd

--- reactApp/addedd/src/App.js
+++ reactApp/addedd/src/App.js
@@ -1,5 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+
+
+class AlexanderArnold extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       count: 0
+    }
+  }
+
+  Increment (){
+    this.setState( x => (
+      {
+        count: x + 1
+      }
+    )
+    )
+  }
+  render() {
+    return (
+      <div>
+        <button onClick={this.Increment}>you clicked this {this.state.count}</button>
+      </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
