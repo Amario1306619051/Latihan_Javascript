44d4eb7e74bbb8e8372765919d5addc158f5265d
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-17 21:53:02 +0700

    props

--- reactApp/propscurlybraces/src/App.js
+++ reactApp/propscurlybraces/src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
 
+function Pro (props) {
+  return(
+    <div>
+      <h3>Hello {props.name} form {props.place}</h3>
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      
+      <h1>Hello World</h1>
+      <Pro name="Amario" place="Bekasi"/>
+      <Pro name="Muhlis" place="Mampang"/>
     </div>
   );
 }
