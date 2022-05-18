92ba02fa129097b6d3347ec1474c97407986acfb
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-18 14:05:39 +0700

    useeffect

--- reactApp/mencobauseefect/src/App.js
+++ reactApp/mencobauseefect/src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import React, {useState, useEffect} from 'react'
+
+function Satu() {
+
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    document.title = `junyuk`
+  })
+  return(
+    <div>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count +1)}> Press me</button>
+    </div>
+  )
+
+}
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Satu />
     </div>
   );
 }
