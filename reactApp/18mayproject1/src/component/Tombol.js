d5f1b63130d497a786e476e9af5e6a265161ab93
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-18 16:21:16 +0700

    add tombol

---
+++ reactApp/18mayproject1/src/component/Tombol.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Tombol() {
+  return (
+    <div>
+        <button className='merah'> Musim dingin</button>
+        <button className='biru'> Musim Panas</button>
+    </div>
+  )
+}
+
+export default Tombol
--- reactApp/18mayproject1/src/component/satu.js
+++ reactApp/18mayproject1/src/component/satu.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './satu.css'
+import Tombol from './Tombol'
 
 export default function Satu() {
   return (
     <div>
         <h1 className='kemarau'>Musim Panas</h1>
+        <Tombol />
     </div>
   )
 }
