684b7b39d1b36feb8a3521c1f265597081cfd917
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-21 01:56:27 +0700

    told you

--- reactApp/forinappjs/src/App.js
+++ reactApp/forinappjs/src/App.js
@@ -2,8 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+   const x = [1,2,3,4]
   return (
     <div className="App">
+      <button> Tambah</button>
+      {
+        x.map(fall => (
+          <h2>{fall}</h2>
+        ))
+      }
     </div>
   );
 }
