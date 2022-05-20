09ccd200dd8d629e8047175b0df3d8e96b28b0b8
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-21 01:00:41 +0700

    project selesai

--- reactApp/button/src/component/tombol.js
+++ reactApp/button/src/component/tombol.js
@@ -1,11 +1,20 @@
 import React, {useState} from 'react'
 
 function Tombol() {
-    const [tema,setTema] = useState('panas')
+    const [tema,setTema] = useState({Theme: 'panas'})
+    
+    const Gantian = () => {
+        if(tema.Theme === 'panas'){
+            setTema({Theme: 'dingin'})
+        }else{
+            setTema({Theme: 'panas'})
+        }
+    }
+
   return (
     <div>
-        <button> Klik disini untuk ganti tema</button>
-        <h4>Tema saat ini {tema}</h4>
+        <button onClick={() => Gantian()}> Klik disini untuk ganti tema</button>
+        <h4>Tema saat ini : {tema.Theme}</h4>
     </div>
   )
 }
